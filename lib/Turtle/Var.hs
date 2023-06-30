56f000520c07a8c2e069723d65a6f3805d44ef92
Archaversine <adam.brohl.w@gmail.com>
2023-06-30 12:11:29 -0500

    Added documentation to Turtle.Var

--- lib/Turtle/Var.hs
+++ lib/Turtle/Var.hs
@@ -11,30 +11,33 @@ import Control.Monad.Writer
 
 import Turtle.Types
 
+-- | Declare a local variable 
 declareLocal :: String -> Turtle (TVal TurtleVar)
 declareLocal name = do 
     tell $ "local " <> name <> "\n"
     return $ TTVar name
 
+-- | Define a local variable
 defineLocal :: ToTVal a b => String -> a -> Turtle (TVal TurtleVar)
 defineLocal name value = do 
     tell $ "local " <> name <> " = " <> showTVal value <> "\n"
     return $ TTVar name
 
+-- | Modify the value of a local variable
 setVar :: ToTVal a b => TVal TurtleVar -> a -> Turtle ()
 setVar (TTVar name) value = tell $ name <> " = " <> showTVal value <> "\n"
 
-(+=) :: NumericTVal a a' => TVal TurtleVar -> a -> Turtle ()
+(+=) :: NumericTVal num a' => TVal TurtleVar -> num -> Turtle ()
 x += value = setVar x $ x .+ value
 
-(-=) :: NumericTVal a a' => TVal TurtleVar -> a -> Turtle ()
+(-=) :: NumericTVal num a' => TVal TurtleVar -> num -> Turtle ()
 x -= value = setVar x $ x .- value
 
-(*=) :: NumericTVal a a' => TVal TurtleVar -> a -> Turtle ()
+(*=) :: NumericTVal num a' => TVal TurtleVar -> num -> Turtle ()
 x *= value = setVar x $ x .* value
 
 -- Using //= instead of /= to not conflict with Prelude./=
-(//=) :: NumericTVal a a' => TVal TurtleVar -> a -> Turtle ()
+(//=) :: NumericTVal num a' => TVal TurtleVar -> num -> Turtle ()
 x //= value = setVar x $ x ./ value
 
 infixr 0 += 

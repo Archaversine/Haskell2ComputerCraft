2fe15265f19d832f302a734e5b841b5aaac15e79
Archaversine <adam.brohl.w@gmail.com>
2023-06-30 11:10:49 -0500

    Added documentation to Dig

--- lib/Turtle/World/Blocks.hs
+++ lib/Turtle/World/Blocks.hs
@@ -10,15 +10,21 @@ instance Show ToolSide where
     show RightSide = "right"
 
 class Dig a where 
+    
+    -- | Breaks the block in front. With hoe: tills dirt in front of it.
     dig      :: a -> TVal Bool 
+
+    -- | Breaks the block above.
     digUp    :: a -> TVal Bool 
+
+    -- | Breaks the block below. With hoe: tills the dirt beneat the space below it.
     digDown  :: a -> TVal Bool
 
-    digM     :: a -> Turtle ()
-    digM     = turtle . dig
+    digM :: a -> Turtle ()
+    digM = turtle . dig
 
-    digUpM   :: a -> Turtle ()
-    digUpM   = turtle . digUp
+    digUpM :: a -> Turtle ()
+    digUpM = turtle . digUp
 
     digDownM :: a -> Turtle ()
     digDownM = turtle . digDown
